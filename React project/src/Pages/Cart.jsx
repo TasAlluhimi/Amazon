dbc13375ca02dbaf1c58ff12d33992bd9b4dff85
tasalluhimi <tasneem.luhimi@gmail.com>
2023-12-17 14:54:45 +0300

    edit

--- React project/src/Pages/Cart.jsx
+++ React project/src/Pages/Cart.jsx
@@ -93,7 +93,16 @@ function Cart() {
         
         
         {data.map((item)=>(
-            <div className='flex w-full bg-white p-3'>
+
+            <div class="container mx-auto px-4 py-8">
+    <div class="flex flex-col md:flex-row md:justify-between md:items-center">
+        <h1 class="text-2xl font-bold my-4">Shopping Cart</h1>
+        <button class="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+      Checkout
+    </button>
+    </div>
+    <div class="mt-8">
+        <div class="flex flex-col md:flex-row border-b border-gray-400 py-4">
             <div class="flex-shrink-0">
                 <img src={item.image} alt="Product image" class="w-32 h-32 object-cover"/>
             </div>
@@ -103,8 +112,8 @@ function Cart() {
                 <div class="mt-4 flex items-center">
                     <span class="mr-2 text-gray-600">Quantity:</span>
                     <div class="flex items-center">
-                        <button class="bg-gray-200 rounded-l-lg px-2 py-1" 
-                        onClick={()=>{del(item.id)}}
+                    <button class="bg-gray-200 rounded-l-lg px-2 py-1" 
+                         onClick={()=>{del(item.id)}}
                         >-</button>
                         <span class="mx-2 text-gray-600">{data.quantity}1</span>
                         <button class="bg-gray-200 rounded-r-lg px-2 py-1"
@@ -113,7 +122,11 @@ function Cart() {
                     <span class="ml-auto font-bold">${data.price}10</span>
                 </div>
             </div>
-            </div>
+        </div>
+
+    </div>
+   
+</div>
         ))}
             
 
